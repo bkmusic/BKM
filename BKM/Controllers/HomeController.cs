0061ffa7db31b9bcf833e100d13adb416149f5f0
Minh <minh.nha162733@sis.hust.edu.vn>
2018-12-27 01:44:10 +0700

    BKMusic

---
+++ BKM/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BKM.Models;
+namespace BKM.Controllers
+{
+    public class HomeController : Controller
+    {
+        bkmusicEntities db = new bkmusicEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+   
+
+        public ActionResult NhacCaNhan()
+        {
+            ViewBag.Message = "This is your music";
+           
+            return View();
+
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact us at";
+
+            return View();
+        }
+    }
+}
