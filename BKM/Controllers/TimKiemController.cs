e9ed56d4c59b5bf56728e70c3bdff97321e38f73
Minh <minh.nha162733@sis.hust.edu.vn>
2018-12-31 07:05:15 +0700

    abc

---
+++ BKM/Controllers/TimKiemController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BKM.Models;
+using BKM.dal;
+
+namespace BKM.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        BKMContext db = new BKMContext();
+        // GET: TimKiem
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TimKiem(FormCollection f)
+        {
+            
+            string sch = f.Get("search").ToString();
+            if (sch != null && sch != "")
+            {
+                var baiHat = db.BaiHats.Take(20).Where(x => x.TenBaiHat.ToUpper().Contains(sch.ToUpper())).ToList();
+                ViewBag.SchCaSi = db.CaSies.Where(x => x.TenCaSi.ToUpper().Contains(sch.ToUpper())).ToList();
+                return View(baiHat);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
+        }
+    }
+}
