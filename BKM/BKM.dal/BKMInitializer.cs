366256de51ea11fbe51028217e24f0cdff10ba8f
etnalo <etnalo.100@gmail.com>
2019-01-02 05:16:42 +0700

    1111

--- BKM/BKM.dal/BKMInitializer.cs
+++ BKM/BKM.dal/BKMInitializer.cs
@@ -14,49 +14,24 @@ namespace BKM.dal
         {
             var caSies = new List<CaSi>
             {
-                new CaSi{TenCaSi ="Default", MoTa ="", HinhAnh="181668.png"},
-                new CaSi{TenCaSi ="Ariana Grande", MoTa ="", HinhAnh="a3-15460480392821349991640.jpg"},
-                new CaSi{TenCaSi ="B Ray", MoTa ="",  HinhAnh="9ce68f7d32102e37837bf3d87c82895c.jpg"},
-                new CaSi{TenCaSi ="Black Pink",MoTa ="",  HinhAnh="1747927_1.jpg"},
-                new CaSi{TenCaSi ="Đạt G",MoTa ="",  HinhAnh="Datg-0.jpg"},
-                new CaSi{TenCaSi ="ERIK	1",MoTa ="",  HinhAnh="1474010206663_600.jpg"},
-                new CaSi{TenCaSi ="Hương Giang",MoTa ="", HinhAnh="16-1520665091402914466665.jpg"},
-                new CaSi{TenCaSi ="Jennie", MoTa ="",  HinhAnh="jennie-22.jpg"}
             };
             caSies.ForEach(s => context.CaSies.Add(s));
             context.SaveChanges();
 
             var TheLoais = new List<TheLoai>
             {
-                new TheLoai{TenTheLoai="Classical"},
-                new TheLoai{TenTheLoai="Country"},
-                new TheLoai{TenTheLoai="Electronic"},
-                new TheLoai{TenTheLoai="Indie"},
-                new TheLoai{TenTheLoai="Jazz"},
-                new TheLoai{TenTheLoai="Pop"},
-                new TheLoai{TenTheLoai="R&B"},
-                new TheLoai{TenTheLoai="Rock"},
             };
             TheLoais.ForEach(s => context.TheLoais.Add(s));
             context.SaveChanges();
 
             var KhuVucs = new List<KhuVuc>
             {
-                 new KhuVuc{TenKhuVuc="Viet Nam"},
-                 new KhuVuc{TenKhuVuc="Au My"},
-                 new KhuVuc{TenKhuVuc="Han Quoc"}
             };
             KhuVucs.ForEach(s => context.KhuVucs.Add(s));
             context.SaveChanges();
 
             var BaiHats = new List<BaiHat>
-            {
-                new BaiHat{TenBaiHat="Anh Đang Ở Đâu Đấy Anh", MaTheLoai=1, MaCaSi = 1, MaKhuVuc= 1,MoTa="", HinhAnh="images.png", File="Anh-Dang-O-Dau-Day-Anh-Huong-Giang.mp3" },
-                new BaiHat{TenBaiHat="Thank U, Next", MaTheLoai=1, MaKhuVuc= 2,MaCaSi = 1, HinhAnh="images.png", File="Thank U_ Next - Ariana Grande [128kbps_MP3].mp3" },
-                new BaiHat{TenBaiHat="Ve", MaTheLoai= 1  , MaKhuVuc=  1  ,MaCaSi = 1, HinhAnh="images.png", File=" Ve-Dat-G-DuUyen-BeeBB.mp3" },
-                new BaiHat{TenBaiHat="Cham Day Noi Dau",  MaTheLoai=  1, MaKhuVuc =  1 , MaCaSi = 1,HinhAnh="images.png", File="Cham -Day-Noi-Dau-ERIK.mp3"} ,
-                new BaiHat{TenBaiHat="Mình Chia Tay Đi" , MaTheLoai=  1,  MaKhuVuc = 1, MaCaSi = 1,HinhAnh="images.png", File="Minh -Chia-Tay-Di-ERIK.mp3"},
-                new BaiHat{TenBaiHat="SoLo" ,  MaTheLoai= 1, MaKhuVuc =3, MaCaSi = 1, HinhAnh="images.png", File = "Solo - Jennie [128kbps_MP3].mp3" }
+            {            
             };
             BaiHats.ForEach(s => context.BaiHats.Add(s));
             context.SaveChanges();
